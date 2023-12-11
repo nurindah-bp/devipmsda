a9ee5543422e9c971bf105ea937f12b67853f7de
nurindah-bp <nurindahbp00@gmail.com>
2023-12-11 15:40:38 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,53 @@
+<!-- Header -->
+
+<header class="header">
+			
+            <!-- Top Bar -->
+            <!--  -->
+    
+            <!-- Header Content -->
+            <div class="header_container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="header_content d-flex flex-row align-items-center justify-content-start">
+                                <div class="logo_container mr-auto">
+                                    <a href="#">
+                                        <div class="logo_text">IPM SIDOARJO</div>
+                                    </a>
+                                </div>
+                                <nav class="main_nav_contaner">
+                                    <ul class="main_nav">
+                                        <li class="active"><a href="index.html">Beranda</a></li>
+                                        <li><a href="blog.html">Berita</a></li>
+                                        <li><a href="courses.html">Produk</a></li>
+                                        <li><a href="contact.html">Kontak</a></li>
+                                    </ul>
+                                </nav>
+                                <div class="header_content_right ml-auto text-right">
+                                    <div class="header_search">
+                                        <div class="search_form_container">
+                                            <form action="#" id="search_form" class="search_form trans_400">
+                                                <input type="search" class="header_search_input trans_400" placeholder="Cari apaa?" required="required">
+                                                <div class="search_button">
+                                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+    
+                                    <!-- Hamburger -->
+    
+                                    <!-- <div class="user"><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></div>
+                                    <div class="hamburger menu_mm">
+                                        <i class="fa fa-bars menu_mm" aria-hidden="true"></i>
+                                    </div> -->
+                                </div>
+    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        </header>
